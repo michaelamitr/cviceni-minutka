51e05945aea15acf59f5e392e78b8f309122e62e
Michaela Trčková <misa.trckova@volny.cz>
2023-04-05 23:29:10 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('funguju!');
+const userSetTime = prompt('Zadejte požadovaný čas (v sekundách):');
+const ms = userSetTime * 1000;
+const ring = () => {
+  document.querySelector('.alarm').classList.add('alarm--ring');
+  const audio = document.querySelector('audio');
+  audio.play();
+};
+setTimeout(ring, ms);
